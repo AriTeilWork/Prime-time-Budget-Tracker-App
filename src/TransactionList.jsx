7dee03f361bccf0efc21d346d5b22147ee0b5b42
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 21:06:26 +0200

    Add basic structure for TransactionList component

---
+++ src/TransactionList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TransactionList = ({ transactions }) => {
+    return (
+        <div>
+            <h3>Transaction List</h3>
+            <ul>
+                {/* Transactions will be rendered here */}
+            </ul>
+        </div>
+    );
+};
+
+export default TransactionList;
