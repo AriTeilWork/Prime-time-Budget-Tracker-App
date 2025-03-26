659d65159caf277037f98295a22761e3a28637dc
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 18:51:54 +0200

    app() fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ function App() {
   const [transactions, setTransactions] = useState([]);
   const [text, setText] = useState('');
   const [amount, setAmount] = useState('');
-} 
+
 // Function to remove a transaction
 function removeTransaction(id) {
   setTransactions(transactions.filter(transaction => transaction.id !== id));
   updateLocalStorage();
   updateValues();
+}
 }
